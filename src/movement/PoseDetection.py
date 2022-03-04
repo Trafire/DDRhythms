bf18799363af5442f729b675d475df0de8b187b8
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2022-03-04 17:19:45 -0500

    update VideoInput

--- src/movement/PoseDetection.py
+++ src/movement/PoseDetection.py
@@ -3,24 +3,6 @@
 import cv2 as cv
 
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument(
-#     "--input", help="Path to image or video. Skip to capture frames from camera"
-# )
-# parser.add_argument(
-#     "--thr", default=0.2, type=float, help="Threshold value for pose parts heat map"
-# )
-# parser.add_argument(
-#     "--width", default=368, type=int, help="Resize input to specific width."
-# )
-# parser.add_argument(
-#     "--height", default=368, type=int, help="Resize input to specific height."
-# )
-# # parser.add_argument('--scale', default=368, type=int, help='Resize input to specific height.')
-#
-# args = parser.parse_args()
-
-
 class VideoInput:
     BODY_PARTS = {
         "Nose": 0,
@@ -60,8 +42,6 @@ class VideoInput:
         ["Neck", "Nose"],
         ["Nose", "REye"],
         ["REye", "REar"],
-        ["Nose", "LEye"],
-        ["LEye", "LEar"],
     ]
 
     def __init__(self, input=0, width=200, height=200, scale=200, thr=0.2):
@@ -74,7 +54,60 @@ class VideoInput:
         self.cap = cv.VideoCapture(self.input)
         self.calc_timestamps = [0.0]
 
-    def get_height(self, points):
+        self._person_height = None
+
+    @staticmethod
+    def get_body_part_location(points: list, part: str) -> None:
+        """Returns value of location of body part
+        Parameters
+        ----------
+        points: List of Intergers
+        part: String in
+
+        Returns float
+        -------
+        """
+        index = VideoInput.BODY_PARTS.get(part, None)
+        # error if body part is not in Body Part List
+        assert index is not None
+        return points[index]
+
+    def person_height(self, points):
+        if self._person_height is None:
+            ## olivias code
+            pass
+            # self._person_height = value
+        return self._person_height
+
+    def height(self, points):
+        ankle = self.get_part_value(points, "LAnkle")
+        knee = self.get_part_value(points, "LKnee")
+
+    def shoulder_width(self, points):
+        pass
+
+    def leg_length(self, points):
+        pass
+
+    def left_foot_position(self, offset):
+        pass
+
+    def right_foot_position(self, offset):
+        pass
+
+    def change_right_in_foot_position(self, offset):
+        pass
+
+    def change_left_in_foot_position(self, offset):
+        pass
+
+    def left_knee(self, offset):
+        pass
+
+    def right_knee(self, offset):
+        pass
+
+    def hip_angle(self, offset):
         pass
 
     def get(self):
@@ -117,10 +150,6 @@ class VideoInput:
                 y = (frameHeight * point[1]) / out.shape[2]
                 # Add a point if it's confidence is higher than threshold.
                 points.append((int(x), int(y)) if conf > self.thr else None)
-
-            print(points)
-            height = self.get_height(points)
-
             for pair in VideoInput.POSE_PAIRS:
                 partFrom = pair[0]
                 partTo = pair[1]
@@ -151,87 +180,9 @@ class VideoInput:
             )
 
             self.calc_timestamps.append(int(self.cap.get(cv.CAP_PROP_POS_MSEC)))
-            print(points)
-            print(self.calc_timestamps[-1])
+
             cv.imshow("OpenPose using OpenCV", frame)
 
 
-v = VideoInput("data/test_video.mp4")
+v = VideoInput(r"C:\Users\Antoine\PycharmProjects\DDRhythms\data\test_video.mp4")
 v.get()
-
-#
-#
-#
-#
-# # inScale = args.scale
-#
-# net = cv.dnn.readNetFromTensorflow(r"C:\Users\Antoine\PycharmProjects\DDRhythms\src\movement\graph_opt.pb")
-#
-# # args.input = 0
-#
-# cap = cv.VideoCapture(args.input if args.input else 0)
-#
-# if cap.isOpened() == False:
-#     print("Error opening video stream or file")
-#
-# while cv.waitKey(1) < 0:
-#     hasFrame, frame = cap.read()
-#     if not hasFrame:
-#         cv.waitKey()
-#         break
-#
-#     frameWidth = frame.shape[1]
-#     frameHeight = frame.shape[0]
-#
-#     net.setInput(
-#         cv.dnn.blobFromImage(
-#             frame,
-#             1.0,
-#             (in_weight, in_height),
-#             (127.5, 127.5, 127.5),
-#             swapRB=True,
-#             crop=False,
-#         )
-#     )
-#     out = net.forward()
-#     out = out[
-#           :, :19, :, :
-#           ]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
-#
-#     assert len(BODY_PARTS) == out.shape[1]
-#
-#     points = []
-#     for i in range(len(BODY_PARTS)):
-#         # Slice heatmap of corresponging body's part.
-#         heatMap = out[0, i, :, :]
-#
-#         # Originally, we try to find all the local maximums. To simplify a sample
-#         # we just find a global one. However only a single pose at the same time
-#         # could be detected this way.
-#         _, conf, _, point = cv.minMaxLoc(heatMap)
-#         x = (frameWidth * point[0]) / out.shape[3]
-#         y = (frameHeight * point[1]) / out.shape[2]
-#         # Add a point if it's confidence is higher than threshold.
-#         points.append((int(x), int(y)) if conf > args.thr else None)
-#
-#     for pair in POSE_PAIRS:
-#         partFrom = pair[0]
-#         partTo = pair[1]
-#         assert partFrom in BODY_PARTS
-#         assert partTo in BODY_PARTS
-#
-#         idFrom = BODY_PARTS[partFrom]
-#         idTo = BODY_PARTS[partTo]
-#
-#         if points[idFrom] and points[idTo]:
-#             cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-#             cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-#             cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-#
-#     t, _ = net.getPerfProfile()
-#     freq = cv.getTickFrequency() / 1000
-#     cv.putText(
-#         frame, "%.2fms" % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0)
-#     )
-#
-#     cv.imshow("OpenPose using OpenCV", frame)
